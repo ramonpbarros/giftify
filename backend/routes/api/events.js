e81f9c258d25f9e9e57675cc2d2c4529bc6e4b64
Ramon <ramonpbarros@gmail.com>
2024-04-08 22:03:04 -0700

    create new event implemented

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { Op } = require('sequelize');
-const { Event } = require('../../db/models');
-const event = require('../../db/models/event');
+const { Event, User } = require('../../db/models');
+const { requireAuth } = require('../../utils/auth');
 
 const router = express.Router();
 
 // Get all Events
-router.get('/', async (req, res) => {
+router.get('/', requireAuth, async (req, res) => {
   try {
     let page = parseInt(req.query.page);
     let size = parseInt(req.query.size);
@@ -74,6 +73,106 @@ router.get('/', async (req, res) => {
     console.error('Error fetching events:', error);
     res.status(500).json({ message: 'Internal server error' });
   }
+});
+
+// Get details of an Event specified by its id
+router.get('/:eventId', requireAuth, async (req, res) => {
+  const event = await Event.findByPk(req.params.eventId, {
+    // include: [{ model: User }],
+  });
+
+  if (!event) {
+    res.status(404);
+    res.send({ message: "Event couldn't be found" });
+  }
+
+  const user = await User.findByPk(event.userId, {
+    attributes: { exclude: ['firstName', 'lastName', 'bio', 'imgUrl'] },
+  });
+
+  const formattedUser = user.toJSON();
+
+  const formattedEvent = event.toJSON();
+
+  const newTimeUpdatedAt = new Date(formattedEvent.updatedAt)
+    .toISOString()
+    .split('')
+    .slice(11, 19)
+    .join('');
+
+  const newDateUpdatedAt = new Date(formattedEvent.updatedAt)
+    .toISOString()
+    .split('T')[0];
+
+  const newTimeCreatedAt = new Date(formattedEvent.createdAt)
+    .toISOString()
+    .split('')
+    .slice(11, 19)
+    .join('');
+
+  const newDateCreatedAt = new Date(formattedEvent.createdAt)
+    .toISOString()
+    .split('T')[0];
+
+  const newDateEventDate = new Date(formattedEvent.eventDate)
+    .toISOString()
+    .split('T')[0];
+
+  delete formattedEvent.createdAt;
+  delete formattedEvent.updatedAt;
+  delete formattedEvent.eventDate;
+
+  formattedEvent.createdAt = `${newDateCreatedAt} ${newTimeCreatedAt}`;
+  formattedEvent.updatedAt = `${newDateUpdatedAt} ${newTimeUpdatedAt}`;
+  formattedEvent.eventDate = `${newDateEventDate}`;
+  formattedEvent.Organizer = formattedUser;
+
+  res.json(formattedEvent);
+});
+
+// Create Event for the Current User
+router.post('/', requireAuth, async (req, res) => {
+  const { eventName, eventDescription, eventDate, private } = req.body;
+
+  const currentUser = req.user.toJSON();
+
+  const newEvent = await Event.create({
+    userId: currentUser.id,
+    eventName,
+    eventDescription,
+    eventDate,
+    private,
+  });
+
+  let currentNewEvent = newEvent.toJSON();
+
+  const newTimeUpdatedAt = new Date(currentNewEvent.updatedAt)
+    .toISOString()
+    .split('')
+    .slice(11, 19)
+    .join('');
+
+  const newDateUpdatedAt = new Date(currentNewEvent.updatedAt)
+    .toISOString()
+    .split('T')[0];
+
+  const newTimeCreatedAt = new Date(currentNewEvent.createdAt)
+    .toISOString()
+    .split('')
+    .slice(11, 19)
+    .join('');
+
+  const newDateCreatedAt = new Date(currentNewEvent.createdAt)
+    .toISOString()
+    .split('T')[0];
+
+  delete formattedEvent.createdAt;
+  delete formattedEvent.updatedAt;
+
+  currentNewEvent.createdAt = `${newDateCreatedAt} ${newTimeCreatedAt}`;
+  currentNewEvent.updatedAt = `${newDateUpdatedAt} ${newTimeUpdatedAt}`;
+
+  res.status(201).json(currentNewEvent);
 });
 
 module.exports = router;
