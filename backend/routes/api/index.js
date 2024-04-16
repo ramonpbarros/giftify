e50b02eb8e78021214ed55e7ad0505ccd9640230
Ramon <ramonpbarros@gmail.com>
2024-04-15 22:26:42 -0700

    attendee route file removed

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const eventsRouter = require('./events.js');
-const attendeesRouter = require('./attendees.js');
 const wishlistsRouter = require('./wishlists.js');
 const { restoreUser } = require('../../utils/auth.js');
 
@@ -11,7 +10,6 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 router.use('/events', eventsRouter);
-router.use('/attendees', attendeesRouter);
 router.use('/wishlists', wishlistsRouter);
 
 router.post('/test', (req, res) => {
