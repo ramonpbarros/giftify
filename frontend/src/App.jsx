85d4d08b2be29b1f8edf7204dbf33999187c1a89
Ramon <ramonpbarros@gmail.com>
2024-04-17 21:03:48 -0700

    navbar component created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import LoginFormPage from './components/LoginFormPage';
 import SignupFormPage from './components/SignupFormPage';
+import Navigation from './components/Navigation';
 import * as sessionActions from './store/session';
 
 function Layout() {
@@ -15,7 +16,12 @@ function Layout() {
     });
   }, [dispatch]);
 
-  return <>{isLoaded && <Outlet />}</>;
+  return (
+    <>
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && <Outlet />}
+    </>
+  );
 }
 
 const router = createBrowserRouter([
@@ -27,11 +33,11 @@ const router = createBrowserRouter([
         element: <h1>Welcome!</h1>,
       },
       {
-        path: '/login',
+        path: 'login',
         element: <LoginFormPage />,
       },
       {
-        path: '/signup',
+        path: 'signup',
         element: <SignupFormPage />,
       },
     ],
