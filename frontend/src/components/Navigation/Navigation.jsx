626ab7c89af3b094dd0bc1a9b17254f44f11500f
Ramon <ramonpbarros@gmail.com>
2024-04-18 23:40:57 -0700

    initiated navbar changes

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,22 +1,54 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector((state) => state.session.user);
+  const navigate = useNavigate();
 
-  return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      {isLoaded && (
-        <li>
-          <ProfileButton user={sessionUser} />
+  return sessionUser ? (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <NavLink
+            to="/"
+            className="navbar-link"
+            activeClassName="navbar-link-active"
+          >
+            Giftify
+          </NavLink>
         </li>
-      )}
-    </ul>
+        {isLoaded && (
+          <li className="navbar-item">
+            <ProfileButton user={sessionUser} />
+          </li>
+        )}
+      </ul>
+    </nav>
+  ) : (
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <NavLink
+            to="/"
+            className="navbar-link"
+            activeClassName="navbar-link-active"
+          >
+            Giftify
+          </NavLink>
+        </li>
+        {isLoaded && (
+          // <li className="navbar-item">
+          //   <ProfileButton user={sessionUser} />
+          //   <button></button>
+          // </li>
+          <div className="btn-login" onClick={() => navigate('/login')}>
+            Login
+          </div>
+        )}
+      </ul>
+    </nav>
   );
 }
 
--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,18 +1,22 @@
 import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
-import { FaUserCircle } from 'react-icons/fa';
+import { FaRegUserCircle } from 'react-icons/fa';
 import * as sessionActions from '../../store/session';
-import OpenModalMenuItem from './OpenModalMenuItem';
+// import OpenModalMenuItem from './OpenModalMenuItem';
 import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from '../SignupFormModal';
+import { Link, useNavigate } from 'react-router-dom';
+import './Navigation.css';
+import OpenModalButton from '../OpenModalButton/OpenModalButton';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [showMenu, setShowMenu] = useState(false);
   const ulRef = useRef();
 
   const toggleMenu = (e) => {
-    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+    e.stopPropagation();
     setShowMenu(!showMenu);
   };
 
@@ -36,39 +40,68 @@ function ProfileButton({ user }) {
     e.preventDefault();
     dispatch(sessionActions.logout());
     closeMenu();
+    navigate('/');
   };
 
   const ulClassName = 'profile-dropdown' + (showMenu ? '' : ' hidden');
 
   return (
     <>
-      <button onClick={toggleMenu}>
-        <FaUserCircle />
+      <button className="custom-button" onClick={toggleMenu}>
+        <FaRegUserCircle />
       </button>
       <ul className={ulClassName} ref={ulRef}>
         {user ? (
           <>
-            <li>{user.username}</li>
-            <li>
-              {user.firstName} {user.lastName}
-            </li>
-            <li>{user.email}</li>
-            <li>
-              <button onClick={logout}>Log Out</button>
-            </li>
+            <div className="list">
+              <div>
+                <p>Hello, {user.username}</p>
+                <p className="email">{user.email}</p>
+              </div>
+              <Link
+                className="nav-btn2"
+                to="events/current"
+                onClick={(e) => {
+                  e.preventDefault();
+                  closeMenu();
+                  navigate('events/current');
+                }}
+              >
+                Manage Spots
+              </Link>
+              <div className="custom-hr"></div>
+              <hr />
+              <button className="nav-btn" onClick={logout}>
+                Log Out
+              </button>
+            </div>
           </>
         ) : (
+          // <>
+          //   <li>{user.username}</li>
+          //   <li>
+          //     {user.firstName} {user.lastName}
+          //   </li>
+          //   <li>{user.email}</li>
+          //   <li>
+          //     <button onClick={logout}>Log Out</button>
+          //   </li>
+          // </>
           <>
-            <OpenModalMenuItem
-              itemText="Log In"
-              onItemClick={closeMenu}
-              modalComponent={<LoginFormModal />}
-            />
-            <OpenModalMenuItem
-              itemText="Sign Up"
-              onItemClick={closeMenu}
-              modalComponent={<SignupFormModal />}
-            />
+            <li>
+              <OpenModalButton
+                buttonText="Log In"
+                onButtonClick={closeMenu}
+                modalComponent={<LoginFormModal />}
+              />
+            </li>
+            <li>
+              <OpenModalButton
+                buttonText="Sign Up"
+                onButtonClick={closeMenu}
+                modalComponent={<SignupFormModal />}
+              />
+            </li>
           </>
         )}
       </ul>
